61d9d57bc4825af0c7aac8f30d356ed711e264f1
Logan Martlew <logan.martlew@gmail.com>
2022-03-30 14:50:12 +1300

    Added dropdown component

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import SEPractices from '../dummydata/SEPractices';
+
 const optionItems = SEPractices.map(SEPractice => (
   <option key={SEPractice.practice}>{SEPractice.practice}</option>
 ));
+
 const Dropdown = () => {
   return (
     <div>
